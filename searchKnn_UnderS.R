140fb5078cb0fb3c09e0f7661abaa0ce8743cafb
fonkafon <38109415+fonkafon@users.noreply.github.com>
2021-05-17 14:29:15 +0700

    Add files via upload

--- searchKnn_UnderS.R
+++ searchKnn_UnderS.R
@@ -1,14 +1,21 @@
 #search knn #caret
-searchKnn <- function(dfT,knn){
+searchKnn <- function(dfT){
   for (i in 1: nrow(dfT)){
     set.seed(seed)
+    k<-(sqrt(imb))+sqrt(nrow(train))
     kNN <- knn3Train(as.matrix(dfT[-i,-ncol(dfT)]), as.matrix(dfT[i,-ncol(dfT)]), 
-                     cl = as.matrix(dfT[-i,]$label), k = knn)
+                     cl = as.matrix(dfT[-i,]$label), k = k)
     #kNN <- knn3Train(df[-i,-ncol(df)], df[i,-ncol(df)], cl = df[-i,]$label, k = k)
     prob <- attr(kNN,'prob')
     ifelse(i==1,allprob <- prob, allprob <- rbind(allprob,prob))
   }
   dfT$probN <- allprob[,1]
   dfT$probP <- allprob[,2]
+  tmpN <- dfT[(dfT$probN==1)&(dfT$label=='negative'),] 
+  tmpP <- dfT[dfT$label=='positive',]
+  dfT <- rbind(tmpP,tmpN)
+  
+  dfT$probP <- NULL
+  dfT$probN <- NULL
   return(dfT)
 }
